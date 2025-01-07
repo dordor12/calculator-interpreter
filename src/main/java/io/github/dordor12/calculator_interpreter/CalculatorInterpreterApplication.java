7bfa66b97959aa313a616cb3928c36b3224aa057
Dor <doramid1590@gmail.com>
2025-01-07 21:11:28 +0200

    init spring project

---
+++ src/main/java/io/github/dordor12/calculator_interpreter/CalculatorInterpreterApplication.java
@@ -0,0 +1,13 @@
+package io.github.dordor12.calculator_interpreter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorInterpreterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorInterpreterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/dordor12/calculator_interpreter/CalculatorInterpreterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dordor12.calculator_interpreter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorInterpreterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
